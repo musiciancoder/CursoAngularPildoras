0756ffb8f9226f0e14679502a2a4f77236ec1f18
musiciancoder <andresguitarra1980@gmail.com>
2022-03-11 10:19:08 -0300

    video 6

--- src/app/empleado/empleado.component.ts
+++ src/app/empleado/empleado.component.ts
@@ -3,7 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-empleado',
   templateUrl: './empleado.component.html',
+  //template:"<p>Aqui iria un empleado</p>", //ojoque si el codigoinline tiene mas de una linea no va entre comillas simples sino ``
   styleUrls: ['./empleado.component.css']
+  //styles:["p{background-color:red;"]
 })
 export class EmpleadoComponent implements OnInit {
 
